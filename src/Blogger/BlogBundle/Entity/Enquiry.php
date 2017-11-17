8ba0b41d4dbed347b20e2ce59487af15e4934698
AleksandrMyronow <a.myronow@gmail.com>
2017-11-17 13:39:33 +0200

    add form on second page

---
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Виктор
+ * Date: 17.11.2017
+ * Time: 12:12
+ */
+
+namespace Blogger\BlogBundle\Entity;
+
+class Enquiry {
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+    protected $name;
+    protected $email;
+    protected $subject;
+    protected $body;
+}
