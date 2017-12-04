485239351beaac3fdb8b653952ff8091603d6233
AleksandrMyronow <a.myronow@gmail.com>
2017-12-04 19:33:18 +0200

    add blog page in the 3 part

---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aleksandr
+ * Date: 04.12.2017
+ * Time: 17:27
+ */
+
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller.
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog' => $blog,
+        ));
+    }
+}
